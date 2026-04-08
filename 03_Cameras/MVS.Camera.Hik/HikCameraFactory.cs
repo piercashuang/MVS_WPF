d70d3a78005e88a9d0c2bd6f6ec910171a10ddc0
zhanyinhao <1440090140@qq.com>
2026-04-08 21:12:41 +0800

    直接先扫描在工厂模式

---
+++ 03_Cameras/MVS.Camera.Hik/HikCameraFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MVS.Contract.Camera;
+
+namespace MVS.Camera.Hik
+{
+    public class HikCameraFactory : ICameraFactory
+    {
+        public string Name => "Hikvision Camera Factory";
+        public string Version => "1.0.0";
+
+        // 已经不需要在这里写扫描逻辑了
+
+        public ICamera CreateCamera(CameraMetaInfo info)
+        {
+            // 直接根据传入的元信息，实例化控制类
+            // return new HikCamera(info.SerialNumber);
+            throw new NotImplementedException("这里写具体的 new HikCamera 逻辑");
+        }
+    }
+}
